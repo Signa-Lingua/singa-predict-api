d62a2e4d8ff2e731378550a3c92a5c7a63f149c6
ahmad alwi <ahmadalwiam@gmail.com>
2024-06-13 20:33:33 +0700

    refactor: rename LoadModel to Model for clarity and format code with black

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
-import tempfile
 import os
 
-from time import sleep
 from dotenv import load_dotenv
 # from typing import Union
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 
-from predict.predict import LoadModel
+from predict.predict import Model
 from service.gcloud_storage import get_model
 
 load_dotenv()
+
 app = FastAPI()
 
+def model():
+    model_path = get_model()
+    load_model = Model(model_path)
 
-def load_model():
-    load_model = LoadModel(get_model())
     return load_model
 
 
 @app.get("/")
 async def read_root():
     return JSONResponse(
-        content={
-            "error": False, 
-            "message": "ML Prediction API is running!"}, 
-        status_code=200)
+        content={"error": False, "message": "ML Prediction API is running!"},
+        status_code=200,
+    )
 
 
 @app.post("/predict")
-def predict(video: UploadFile = File(
-    media_type=["video/mp4", "video/x-m4v", "video/*"]
-)):
+def predict(
+    video: UploadFile = File(media_type=["video/mp4", "video/x-m4v", "video/*"])
+):
     try:
         # with tempfile.NamedTemporaryFile() as tmp_file:
         #     tmp_file.write(video.file.read())
@@ -40,53 +39,55 @@ def predict(video: UploadFile = File(
         #     result = lm.predict_v(tmp_file.name)
         #     tmp_file.close()
 
-        with open("file.mp4", "wb") as f:
+        with open("./videos/file.mp4", "wb") as f:
             f.write(video.file.read())
 
-            lm = load_model()
-            result = lm.predict_v("file.mp4")
+            lm = model()
 
+            result = lm.process_video("./videos/file.mp4")
 
             f.close()
 
         return JSONResponse(
             content={
-                "error": False, 
-                "message": "Prediction successful!", 
-                "data": result}, 
-            status_code=200)
+                "error": False,
+                "message": "Prediction successful!",
+                "data": result,
+            },
+            status_code=200,
+        )
     except Exception as e:
         return JSONResponse(
-            content={
-                "error": True, 
-                "message": "Prediction failed!", 
-                "data": str(e)}, 
-            status_code=500)
+            content={"error": True, "message": "Prediction failed!", "data": str(e)},
+            status_code=500,
+        )
 
 
 @app.post("/init")
 def init_model():
     try:
         get_model()
+
         return JSONResponse(
-            content={
-                "error": False, 
-                "message": "Model initialized!"}, 
-            status_code=200)
+            content={"error": False, "message": "Model initialized!"}, status_code=200
+        )
     except Exception as e:
         return JSONResponse(
             content={
-                "error": True, 
-                "message": "Model initialization failed!", 
-                "data": str(e)}, 
-            status_code=500)
+                "error": True,
+                "message": "Model initialization failed!",
+                "data": str(e),
+            },
+            status_code=500,
+        )
 
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(
-        "main:app", 
-        host=str(os.getenv('HOST')), 
-        port=int(os.getenv('PORT')),
-        reload=True
-        )
+        "main:app",
+        host=str(os.getenv("HOST")),
+        port=int(os.getenv("PORT")),
+        reload=True,
+    )
